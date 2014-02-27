b2ffaf69f56ee034baf1225ccdcdc5a4f1142395
Andrea Zonca <code@andreazonca.com>
2014-02-26 19:11:56 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 BRANCH=master
-TARGET_REPO=swcarpentry-2014-07-07-esu/swcarpentry-2014-07-07-esu.github.io
+TARGET_REPO=swcarpentry-pelican/swcarpentry-pelican.github.io
 PELICAN_OUTPUT_FOLDER=output
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
@@ -9,8 +9,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
     fi
-    #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    #using token clone github pages repository
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO.git built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
